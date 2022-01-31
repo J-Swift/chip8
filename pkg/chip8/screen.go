bf71e516ddfe0d766a3d6e490e653cdb644e43f9
Jimmy Reichley <jimmyqpublik@gmail.com>
2022-01-31 00:28:54 -0500

    Basic screen.draw

--- pkg/chip8/screen.go
+++ pkg/chip8/screen.go
@@ -1,17 +1,39 @@
 package chip8
 
+import "fmt"
+
 type Screen struct {
-	maxX   int
-	maxY   int
-	pixels []bool
+	columns      int
+	rows         int
+	offRune      rune
+	onRune       rune
+	pixels       []bool
+	screenBuffer []rune
 }
 
 func newScreen() *Screen {
-	screen := Screen{maxX: 64, maxY: 32}
-	screen.pixels = make([]bool, screen.maxX*screen.maxY)
+	screen := Screen{columns: 64, rows: 32, offRune: '⬛', onRune: '🟨'}
+
+	screen.pixels = make([]bool, screen.columns*screen.rows)
+
+	screen.screenBuffer = make([]rune, screen.columns*screen.rows)
+	for i := 0; i < len(screen.screenBuffer); i++ {
+		screen.screenBuffer[i] = screen.offRune
+	}
+
 	return &screen
 }
 
-func (s *Screen) clear() {
-	s.pixels = make([]bool, s.maxX*s.maxY)
+func (s *Screen) Clear() {
+	s.pixels = make([]bool, s.columns*s.rows)
+}
+
+func (s *Screen) Draw() {
+	s.doDraw()
+}
+
+func (s *Screen) doDraw() {
+	for row := 0; row < s.rows; row++ {
+		fmt.Println(string(s.screenBuffer[row*s.columns : row*s.columns+s.columns]))
+	}
 }
